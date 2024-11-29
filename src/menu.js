123710e082b256a5eddd3f5e628e4c110467635c
Rustam Kudratov <kudratov.rustik@gmail.com>
2024-11-29 23:02:12 +0300

    changed context-menu listener action and made all properties private

--- src/menu.js
+++ src/menu.js
@@ -2,34 +2,36 @@ import { Menu } from './core/menu';
 import { components } from './modules/components.module';
 
 export class ContextMenu extends Menu {
+	#$rootElem;
+	#modules;
 	constructor(selector) {
 		super(selector);
-		this.$rootElem = document.querySelector('selector');
+		this.#$rootElem = document.querySelector(selector);
 
-		this.$rootElem.addEventListener('click', (e) => {
+		this.#modules = components;
+
+		this.#$rootElem.addEventListener('click', (e) => {
 			const { type } = e.target.dataset;
-			components[type].trigger();
+			this.#modules[type].trigger();
 		});
 
-		this.modules = components;
-
-		Object.values(this.modules).forEach((module) => {
+		Object.values(this.#modules).forEach((module) => {
 			this.#add(module);
 		});
 	}
 
 	#add(module) {
-		this.$rootElem.insertAdjacentHTML('beforeend', module.toHTML());
+		this.#$rootElem.insertAdjacentHTML('beforeend', module.toHTML());
 	}
 
 	open(coordX, coordY) {
-		this.$rootElem.classList.add('open');
-		const { style } = this.$rootElem;
+		this.#$rootElem.classList.add('open');
+		const { style } = this.#$rootElem;
 		style.top = `${coordY}px`;
 		style.left = `${coordX}px`;
 	}
 
 	close() {
-		this.$rootElem.classList.remove('open');
+		this.#$rootElem.classList.remove('open');
 	}
 }
