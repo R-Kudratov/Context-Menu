04f201998bb497d12174981aaef725394a0b7b05
SoBolEZnuYoU <119670689+SoBolEZnuYoU@users.noreply.github.com>
2024-11-30 15:46:06 +0400

    added countdown-timer module

--- src/modules/components.module.js
+++ src/modules/components.module.js
@@ -1,7 +1,9 @@
 import { BackgroundModule } from './background.module';
 import {CustomMessageModule} from './custom-message.module'
+import { CountdownTimerModule } from './countdown-timer.module';
 
 export const components = {
 	backgroundModule: new BackgroundModule('backgroundModule', 'Поменять цвет фона'),
 	customMessageModule: new CustomMessageModule('customMessageModule', 'Вызвать сообщение'),
+	countdownTimerModule: new CountdownTimerModule('countdownTimerModule', 'Вызвать таймер'),
 };
