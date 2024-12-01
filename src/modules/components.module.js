ca9d50a9da374104f85670b8801248e9f09d549a
Rustam Kudratov <kudratov.rustik@gmail.com>
2024-12-01 17:40:50 +0300

    added random-quote module

--- src/modules/components.module.js
+++ src/modules/components.module.js
@@ -2,10 +2,12 @@ import { BackgroundModule } from './background.module';
 import {CustomMessageModule} from './custom-message.module'
 import { ClicksModule } from './clicks.module'
 import { FigureModule } from './figure.module'
+import { RandomQuoteModule } from './random-quote.module'
 
 export const components = {
 	backgroundModule: new BackgroundModule('backgroundModule', 'Поменять цвет фона'),
 	customMessageModule: new CustomMessageModule('customMessageModule', 'Вызвать сообщение'),
 	clicksModule: new ClicksModule('clicksModule', 'Считать клики (за 5 секунд)'),
 	figureModule: new FigureModule('figureModule', 'Создать фигуру'),
+	randomQuoteModule: new RandomQuoteModule('randomQuoteModule', 'Прочитать цитату'),
 };
---
+++ src/modules/random-quote.module.js
@@ -0,0 +1,73 @@
+import {Module} from '../core/module'
+
+export class RandomQuoteModule extends Module {
+  #$rootElement
+  #API_URL
+  #quoteText
+  #quoteAuthor
+  #elementTimeoutId
+  #animationTimeoutID
+
+  constructor(type, text) {
+    super(type, text)
+    this.#$rootElement = document.createElement('div')
+    this.#$rootElement.classList.add('quote__container', 'with-shadow')
+    this.#quoteText = document.createElement('p')
+    this.#quoteText.classList.add('quote__text')
+    this.#quoteAuthor = document.createElement('p')
+    this.#quoteAuthor.classList.add('quote__author')
+
+    this.#elementTimeoutId = null
+    this.#animationTimeoutID = null
+
+    this.#API_URL = 'https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=json'
+  }
+
+  async #fetchQuote() {
+    try {
+      const response = await fetch(this.#API_URL)
+      if (!response.ok) {
+        throw new Error()
+      }
+      return await response.json()
+    } catch (error) {
+      return null
+    }
+  }
+
+  async trigger() {
+    if (this.#elementTimeoutId) {
+      this.#removeElement(this.#elementTimeoutId)
+    }
+    if (this.#animationTimeoutID) {
+      this.#removeElement(this.#animationTimeoutID)
+    }
+
+    try {
+      const quoteData = await this.#fetchQuote()
+      this.#quoteText.textContent = quoteData.quoteText
+      this.#quoteAuthor.textContent = quoteData.quoteAuthor
+      this.#$rootElement.append(this.#quoteText, this.#quoteAuthor)
+      document.body.appendChild(this.#$rootElement)
+
+      setTimeout(() => {
+        this.#$rootElement.style.opacity = '1'
+      }, 0)
+
+      this.#elementTimeoutId = setTimeout(() => {
+        this.#$rootElement.style.opacity = '0'
+        this.#animationTimeoutID = setTimeout(() => {
+          if (this.#$rootElement) this.#$rootElement.remove()
+        }, 500);
+      }, 10000)
+
+    } catch (error) {
+      console.log('Не удалось получить цитату')
+    }
+  }
+
+  #removeElement(timeoutId) {
+    clearTimeout(timeoutId)
+    this.#$rootElement.remove()
+  }
+}
